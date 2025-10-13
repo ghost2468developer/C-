0d2aa1aa3c5c65079a7b994995406a52f7516fdb
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2025-10-14 00:31:01 +0200

    init

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C#");
+        Console.Write("Enter you name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Hello {name}, you are {age} years old");
+    }
+}
