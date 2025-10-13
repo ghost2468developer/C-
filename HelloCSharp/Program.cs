e2060260be0ee0d2243d883f1c523db518209483
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2025-10-14 00:58:07 +0200

    init

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to C#");
-        Console.Write("Enter you name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter age: ");
-        int age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Hello {name}, you are {age} years old");
+        Console.Write(" How many hours did you work? ");
+        string hoursInput = Console.ReadLine();
+        double hours = Convert.ToDouble(hoursInput);
+
+        Console.Write("What is your hourly rate? ");
+        string rateInput = Console.ReadLine();
+        double rate = Convert.ToDouble(rateInput);
+
+        double total = hours * rate;
+
+        Console.WriteLine($"You earned {total} this week");
     }
 }
--- HelloCSharp/obj/Debug/net9.0/HelloCSharp.AssemblyInfo.cs
+++ HelloCSharp/obj/Debug/net9.0/HelloCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2aa1aa3c5c65079a7b994995406a52f7516fdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
